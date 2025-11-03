17f8058e6272f7401df63fb4e08ce8eb929041b3
Arandelle Paguinto <topaguintsarandell@gmail.com>
2025-11-04 01:01:53 +0800

    fix the router

--- routers/__init__.py
+++ routers/__init__.py
@@ -3,6 +3,6 @@ from .auth import router as auth
 from .transactions import router as transactions
 from .category import router as category
 from .categoryGroup import router as categoryGroup
-from .groupWithCategory import router as fetchCategories
+from .groupWithCategory import router as groupWithCategory
 
 all_routers = [ai_router, auth, transactions, category, categoryGroup, groupWithCategory]
